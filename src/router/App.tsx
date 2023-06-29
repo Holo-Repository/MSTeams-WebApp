ca973f9e5ec8cf62bde044e042280a470787b680
httom123 <huangtao_2001@163.com>
2023-06-29 22:00:53 +0100

    HR-83 Adds app into microsoft team meeting room
    Change the configuration of maifest file to allow app can be load into team meeting, also delete the unused file.

---
+++ src/router/App.tsx
@@ -0,0 +1,8 @@
+// https://fluentsite.z22.web.core.windows.net/quick-start
+/**
+ * The main app which handles the initialization and routing
+ * of the app.
+ */
+export default function App() {
+  return <div>Hello</div>;
+}
