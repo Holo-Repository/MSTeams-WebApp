c1726ebf7e47d75f12e81148f57e295b1219c783
Mike_V <michelangelo.valsecchi@gmail.com>
2023-07-05 16:15:34 +0100

    HR-65 Again. more. comments.

--- src/routes/HoloRepo.tsx
+++ src/routes/HoloRepo.tsx
@@ -3,9 +3,19 @@ import { app } from "@microsoft/teams-js";
 
 import SharedCanvas from "./canvas/SharedCanvas";
 
+
+/**
+ * The HoloRepo component.
+ * This component is responsible for rendering the correct view based on the context.
+ * It is the main component of the app and holds all the reactive logic.
+ * 
+ * The view is determined by the context.page.frameContext value served by the Teams SDK.
+ */
 class HoloRepo extends React.Component {
     state = {
         // Expected values: "default", "content", "sidePanel", "meetingStage"
+        // Represents the current view where the app is running.
+        // Don't forget that multiple views can be running at the same time.
         view: "default",
     };
 
@@ -17,6 +27,7 @@ class HoloRepo extends React.Component {
     render() {
         const { view } = this.state;
         return (
+            // This is a simple conditional rendering based on the view state.
             <div>
                 {view === "default" && "Default view"}
                 {view === "content" && "Content view"}
