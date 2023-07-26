43da3570377dbdbf8fc9f13b166ff97e9e35de0d
Mike_V <michelangelo.valsecchi@gmail.com>
2023-07-26 21:04:42 +0100

    HR-107 Added animation, better arrow and text with Fluent UI

--- src/AppConfig.tsx
+++ src/AppConfig.tsx
@@ -1,10 +1,13 @@
-import React from "react";
 import {app, pages} from "@microsoft/teams-js";
+import { PiArrowDownThin as Arrow } from "react-icons/pi";
+import { Stack } from '@fluentui/react/lib/Stack';
+import { Text, typographyStyles, makeStyles } from "@fluentui/react-components";
 
 import styles from "./styles/AppConfig.module.css";
 
-const arrow = require("./assets/curved-arrow.png");
-
+const useStyles = makeStyles({
+    text: typographyStyles.title2,
+});
 
 /**
  * The 'Config' component is used to display your group tabs
@@ -12,40 +15,49 @@ const arrow = require("./assets/curved-arrow.png");
  * make their choices and once they are done you will need to validate
  * their choices and communicate that to Teams to enable the save button.
  */
-class AppConfig extends React.Component {
-    render() { // Initialize the Microsoft Teams SDK
-        app.initialize().then(() => { /**
-            * When the user clicks "Save", save the url for your configured tab.
-            * This allows for the addition of query string parameters based on
-            * the settings selected by the user.
-            */
-            pages.config.registerOnSaveHandler((saveEvent) => {
-                const baseUrl = `https://${window.location.hostname}:${window.location.port}`;
-                pages.config.setConfig({
-                    suggestedDisplayName: "HoloRepo",
-                    entityId: "Test",
-                    contentUrl: baseUrl + "/index.html#/holorepo",
-                    websiteUrl: baseUrl + "/index.html#/holorepo"
-                }).then(() => {
-                    saveEvent.notifySuccess();
-                });
-            });
+function AppConfig() {
+    const typo = useStyles();
 
-            /**
-             * After verifying that the settings for your tab are correctly
-             * filled in by the user you need to set the state of the dialog
-             * to be valid.  This will enable the save button in the configuration
-             * dialog.
-             */
-            pages.config.setValidityState(true);
+    // Initialize the Microsoft Teams SDK
+    app.initialize().then(() => { /**
+        * When the user clicks "Save", save the url for your configured tab.
+        * This allows for the addition of query string parameters based on
+        * the settings selected by the user.
+        */
+        pages.config.registerOnSaveHandler((saveEvent) => {
+            const baseUrl = `https://${window.location.hostname}:${window.location.port}`;
+            pages.config.setConfig({
+                suggestedDisplayName: "HoloRepo",
+                entityId: "Test",
+                contentUrl: baseUrl + "/index.html#/holorepo",
+                websiteUrl: baseUrl + "/index.html#/holorepo"
+            }).then(() => {
+                saveEvent.notifySuccess();
+            });
         });
 
-        return (
-            <div className={styles.config}>
-                <img src={arrow} alt="arrow" className={styles.arrow} />
-            </div>
-        );
-    }
+        /**
+         * After verifying that the settings for your tab are correctly
+         * filled in by the user you need to set the state of the dialog
+         * to be valid.  This will enable the save button in the configuration
+         * dialog.
+         */
+        pages.config.setValidityState(true);
+    });
+
+    return (
+        <div>
+            <Arrow className={styles.arrow} />
+            <Stack className={styles.stack} tokens={{childrenGap:'l2', padding:'l2'}} verticalAlign="center">
+                <Stack.Item align="center">
+                    <Text className={typo.text}>Nothing to setup</Text>
+                </Stack.Item>
+                <Stack.Item align="center">
+                    <Text className={typo.text}>Press Save and let the ideas flow!</Text>
+                </Stack.Item>
+            </Stack>
+        </div>
+    );
 }
 
 export default AppConfig;
