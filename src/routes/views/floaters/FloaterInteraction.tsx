b00e45a34758bbb637f7c93aa5590651fa8e205b
httom111 <huangtao_2001@163.com>
2023-08-10 05:43:41 +0800

    Fix UI stuff

--- src/routes/views/floaters/FloaterInteraction.tsx
+++ src/routes/views/floaters/FloaterInteraction.tsx
@@ -3,12 +3,14 @@ import {
     Pin12Regular as Pin,
     Delete12Regular as Delete,
     Drag24Regular as Drag,
+    ArrowDownload24Regular as Export,
 } from "@fluentui/react-icons";
 
 
 export interface FloaterInteractionProps {
     delete : () => void;
     drag : (event: any) => void;
+    export: () => void;
 }
 
 function FloaterInteraction(props: FloaterInteractionProps) {
@@ -19,6 +21,8 @@ function FloaterInteraction(props: FloaterInteractionProps) {
             <ToolbarButton icon={<Pin />} title="Pin" as="button" appearance="subtle"/>
             <ToolbarDivider />
             <ToolbarButton icon={<Delete />} title="Delete" as="button" appearance="subtle" onClick={props.delete}/>
+            <ToolbarDivider />
+            <ToolbarButton icon={<Export />} title="Export" as="button" appearance="subtle" onClick={props.export}/>
         </Toolbar>
     );
 } 
