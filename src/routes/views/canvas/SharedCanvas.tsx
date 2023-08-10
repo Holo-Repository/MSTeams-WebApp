dae2094240d7ae7835e3576c133c2605bf234096
Mike_V <michelangelo.valsecchi@gmail.com>
2023-08-10 21:10:30 +0100

    HR-66 Rewrote SharedCanvas as functional component

--- src/routes/views/canvas/SharedCanvas.tsx
+++ src/routes/views/canvas/SharedCanvas.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import html2canvas from "html2canvas";
 import { InkingManager, LiveCanvas } from "@microsoft/live-share-canvas";
 import { IFluidHandle } from "@fluidframework/core-interfaces";
@@ -10,6 +10,7 @@ import '../../../styles/SharedCanvas.css';
 import { IFluidContainer, SharedMap } from "fluid-framework";
 import Floater from "../floaters/Floater";
 import { Dismiss24Filled } from "@fluentui/react-icons";
+import { LocalJWKSet } from "jose/dist/types/jwks/local";
 
 
 
@@ -24,93 +25,77 @@ export interface SharedCanvasProps {
  * This component is responsible for setting up the Fluid container and the inking manager.
  * As well as rendering the drawing manager component in the newly created live canvas.
  */
-class SharedCanvas extends React.Component<SharedCanvasProps> {
-    state = {
-        inkingManager: undefined as InkingManager | undefined,
-        myVisibleTool: undefined as string | undefined,
-        floaterHandles: {} as { [key: string]: IFluidHandle },
-    }
+function SharedCanvas(props: SharedCanvasProps) {
+    const [container, setContainer] = useState<IFluidContainer>();
+    const [inkingManager, setInkingManager] = useState<InkingManager>();
+    const [myVisibleTool, setMyVisibleTool] = useState<string>();
+    const [floaterHandles, setFloaterHandles] = useState<SharedMap>();
+    const [floatersList, setFloatersList] = useState<{key: string, value: IFluidHandle}[]>([]);
     
-    fluentProviderRef = React.createRef<HTMLDivElement>();
-    closeButtonRef = React.createRef<HTMLDivElement>();
-    myToolBarDivRef = React.createRef<HTMLDivElement>();
-    canvas = React.createRef<HTMLDivElement>();
-    floaters = undefined as SharedMap | undefined;
-    container = undefined as IFluidContainer | undefined
+    const canvasRef = useRef<HTMLDivElement>(null);
+    const fluentProviderRef = useRef<HTMLDivElement>(null);
+    const closeButtonRef = useRef<HTMLDivElement>(null);
+    const myToolBarDivRef = useRef<HTMLDivElement>(null);
 
-    /**
-     * Initializes the Fluid container and the inking manager once the component is mounted.
-     */
-    async componentDidMount() {
+    useEffect(() => {
         // Connect to the active Fluid container
-        this.container = (await this.props.containerManager.getContainer(this.props.container)).container;
-        const liveCanvas = this.container.initialObjects.liveCanvas as LiveCanvas;
+        props.containerManager.getContainer(props.container).then((containerManager) => {
+            setContainer(containerManager.container);
+        });
+    }, [props.containerManager, props.container]);
+    
+    useEffect(() => {
+        if (!container || !canvasRef.current) return;
         
-        if (!this.canvas.current) throw new Error("Canvas host not found");
-        const inkingManager = new InkingManager(this.canvas.current);
+        const liveCanvas = container.initialObjects.liveCanvas as LiveCanvas;
+        const inkingManager = new InkingManager(canvasRef.current);
         
         // Begin synchronization for LiveCanvas
-        await liveCanvas.initialize(inkingManager);
+        liveCanvas.initialize(inkingManager);
         inkingManager.referencePoint = 'center';
+        setInkingManager(inkingManager);
+        isPointerSelected(true);
         
-        this.floaters = this.container.initialObjects.floaters as SharedMap;
-        // this.floaters.clear();
-        const floaterHandles: { [key: string]: IFluidHandle } = {};
-        for (const [key, value] of this.floaters.entries()) if (value?.get) floaterHandles[key] = value;
-        setInterval(() => {
-            console.log('# floaters:', this.floaters!.size);
-            this.floaters!.forEach((value, key) => {
-                console.log(`Key: ${key}, Value: ${value}`);
-            });
-        }, 5000);
-        this.floaters.on("valueChanged", this.handleFloaterChange);
+        // Loading floaters
+        const floaters = container.initialObjects.floaters as SharedMap;
 
-        this.isPointerSelected(true);
-        
-        this.setState({
-            inkingManager,
-            floaterHandles,
-        });
+        const handleChange = () => handleFloaterChange(floaters);
+        floaters.on("valueChanged", handleChange);
+        setFloaterHandles(floaters);
+
+        return () => {
+            floaters!.off("valueChanged", handleChange);
+        };
+    }, [container, canvasRef]);
+
+    useEffect(() => {
+        handleFloaterChange(floaterHandles);
+    }, [floaterHandles]);
+
+    const isPointerSelected = (selected: boolean) => {
+        if (canvasRef.current) canvasRef.current.style.pointerEvents = selected ? 'none' : 'auto';
     }
 
-    handleFloaterChange = (changed: any) => {
-        const newValue = this.floaters!.get(changed.key);
-        if (newValue)
-            this.setState({
-                floaterHandles: {
-                    ...this.state.floaterHandles,
-                    [changed.key]: newValue
-                },
-            });
-        else
-            this.setState({
-                floaterHandles: Object.fromEntries(
-                    Object.entries(this.state.floaterHandles).filter(([key, value]) => key !== changed.key)
-                ),
-            });
+    const handleFloaterChange = (handles?: SharedMap) => {
+        if (!handles) return;
+        const handleList = [];
+        for (const [key, value] of handles.entries()) handleList.push({key, value});
+        setFloatersList(handleList);
     }
 
-    deleteFloater = (key: string) => {
-        this.floaters!.delete(key);
+    const deleteFloater = (key: string) => {
+        floaterHandles!.delete(key);
     }
 
-    setVisibleTool = (event: any) => {
-        this.setState({myVisibleTool: event.currentTarget.value})
+    const setVisibleTool = (event: any) => {
+        setMyVisibleTool(event.currentTarget.value);
     }
 
-    getVisibleTool() {
-        return this.state.myVisibleTool;
-    }
-
-    isPointerSelected = (selected: boolean) => {
-        if (this.canvas.current) this.canvas.current.style.pointerEvents = selected ? 'none' : 'auto';
-    }
-
-    canvasToDataUrl = async (scale: number = 1) => {
-        const fluentProviderElement = this.fluentProviderRef.current;
+    const canvasToDataUrl = async (scale: number = 1) => {
+        const fluentProviderElement = fluentProviderRef.current;
         let dataUrl = '';
         if (fluentProviderElement) {
-            const ignoredNodes = [this.myToolBarDivRef.current, this.closeButtonRef.current];
+            const ignoredNodes = [myToolBarDivRef.current, closeButtonRef.current];
             const canvas = await html2canvas(fluentProviderElement as HTMLElement, {
                 ignoreElements: (node) => {
                     return ignoredNodes.includes(node as HTMLDivElement);
@@ -121,62 +106,52 @@ class SharedCanvas extends React.Component<SharedCanvasProps> {
         return dataUrl;
     }
     
-    downloadDataUrlAsPng = (dataUrl: string) => {
+    const downloadDataUrlAsPng = (dataUrl: string) => {
         const a = document.createElement('a');
         a.href = dataUrl;
         a.download = 'canvas.png';
         a.click();
     }
     
-    exportToPng = async () => {
-        const dataUrl = await this.canvasToDataUrl();
-        if (dataUrl) {
-            this.downloadDataUrlAsPng(dataUrl);
-        }
+    const exportToPng = async () => {
+        const dataUrl = await canvasToDataUrl();
+        if (dataUrl) downloadDataUrlAsPng(dataUrl);
     }
 
-    async closeCanvas() {
-        let scale = 1;
-        let imgUrl;
+    const closeCanvas = async () => {
+        let scale = 1; let imgUrl;
         do {
             scale /= 2;
-            imgUrl = await this.canvasToDataUrl(scale);
+            imgUrl = await canvasToDataUrl(scale);
         } while (imgUrl.length > 30720);
         const containerMap = {time: new Date().toISOString(), previewImage: imgUrl};
-        await this.props.containerManager.updateContainerProperty(this.props.container, containerMap);
-        this.props.closeCanvas();
+        await props.containerManager.updateContainerProperty(props.container, containerMap);
+        props.closeCanvas();
     }
 
-
-    render(): React.ReactNode {
-        const { 
-            inkingManager,
-            floaterHandles,
-        } = this.state;
-
-        return (
-            <FluentProvider id='canvas-background' theme={teamsLightTheme} ref={this.fluentProviderRef}>
-                <div ref={this.closeButtonRef} id="close-button">
-                    <Tooltip content="Close Collab Case" relationship="label">
-                        <Button
-                            icon={<Dismiss24Filled color="#424242"/>}
-                            onClick={this.closeCanvas.bind(this)}/>
-                    </Tooltip>
-                </div>
-                <div id="canvas-host" ref={this.canvas} onClick={this.setVisibleTool} />
-                {this.container && <MyToolBar innerDivRef={this.myToolBarDivRef}  ink={inkingManager} container={this.container} pointerSelected={this.isPointerSelected} exportCanvas={this.exportToPng}/>}
-                <div id='floaters' >
-                    {inkingManager && Object.entries(floaterHandles).map(([key, value]) => 
-                        <Floater 
-                            key={key} 
-                            handle={value} delete={() => {this.deleteFloater(key)}}
-                            inkingManager={inkingManager}
-                        />
-                    )}
-                </div>
-            </FluentProvider>
-        );
-    }
+    return (
+        <FluentProvider id='canvas-background' theme={teamsLightTheme} ref={fluentProviderRef}>
+            <div ref={closeButtonRef} id="close-button">
+                <Tooltip content="Close Collab Case" relationship="label">
+                    <Button
+                        icon={<Dismiss24Filled color="#424242"/>}
+                        onClick={closeCanvas}/>
+                </Tooltip>
+            </div>
+            <div id="canvas-host" ref={canvasRef} onClick={setVisibleTool} />
+            {container && <MyToolBar innerDivRef={myToolBarDivRef}  ink={inkingManager} container={container} pointerSelected={isPointerSelected} exportCanvas={exportToPng}/>}
+            <div id='floaters' >
+                {floaterHandles && floatersList.map(({key, value}) => {
+                    return <Floater 
+                        key={key} 
+                        handle={value}
+                        delete={() => {deleteFloater(key)}}
+                        inkingManager={inkingManager!}
+                    />
+                })}
+            </div>
+        </FluentProvider>
+    );
 }
 
 export default SharedCanvas;
