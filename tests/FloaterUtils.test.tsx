a978721abcc04db9834d1ea832b28c6264fe6081
vickysasa <135549956+Viccsasa@users.noreply.github.com>
2023-08-22 13:56:36 +0100

    HR-160: Created test configuration

---
+++ tests/FloaterUtils.test.tsx
@@ -0,0 +1,13 @@
+import { render } from '@testing-library/react';
+import { appToScreenPos } from "../src/routes/views/utils/FloaterUtils";
+import React from 'react';
+
+
+test('appToScreenPos', () => {
+    expect(appToScreenPos).toBeDefined();
+  }
+);
+
+
+
+
---
+++ tests/Tool.test.tsx
@@ -0,0 +1,37 @@
+import { render, screen } from '@testing-library/react';
+import Tool from "../src/routes/views/canvas/toolbar/tools/Tool";
+import React from 'react';
+import { InkingTool } from '@microsoft/live-share-canvas';
+
+
+describe('Tool Component', () => {
+
+    const mockSelectTool = jest.fn();
+    const mockIsSelected = jest.fn();
+    const mockIsDoubleClicked = jest.fn();
+    const mockExt = jest.fn();
+
+    beforeEach(() => {
+        mockSelectTool.mockClear();
+        mockIsSelected.mockClear();
+        mockIsDoubleClicked.mockClear();
+        mockExt.mockClear();
+    });
+
+    it('renders correctly', () => {
+        render(
+            <Tool 
+                icon = "✒️"
+                tool = {InkingTool.pen}
+                isDoubleClicked={mockIsDoubleClicked}
+                isSelected={mockIsSelected}
+                selectTool={mockSelectTool}
+                ext={mockExt}
+            />
+        );
+        expect(screen.getByText("✏️")).toBeInTheDocument();
+    });
+});
+
+export{};
+
